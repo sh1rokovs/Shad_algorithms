92ce24061a6bf4e9d1267d948d2bf1ccf54db409
sh1rokovs <hilallod@yandex.ru>
2022-11-13 19:38:47 +0300

    up

--- block_3/3.cpp
+++ block_3/3.cpp
@@ -1,82 +1,178 @@
 #include <iostream>
-#include <list>
-#include <map>
-#include <vector>
 
-struct request_entry {
-    int size;
-    int id;
+using std::cin;
+using std::cout;
+
+class MemorySegment {
+public:
+    MemorySegment* prev;
+    MemorySegment* next;
+    bool free;
+    int start, end, id;
+
+    MemorySegment(MemorySegment* prev, MemorySegment* next, bool free, int start, int end, int id) {
+        this->prev = prev;
+        this->next = next;
+        this->free = free;
+        this->start = start;
+        this->end = end;
+        this->id = id;
+        if (prev != nullptr) {
+            prev->next = this;
+        }
+        if (next != nullptr) {
+            next->prev = this;
+        }
+    }
+    void remove() const {
+        if (prev != nullptr) {
+            prev->next = next;
+        }
+        if (next != nullptr) {
+            next->prev = prev;
+        }
+    }
 };
 
-map<int, int> free_blocks_id;
-multimap<int, int> free_blocks_size;
+const int maxRequestsNumber = 100000;
+MemorySegment* heap[maxRequestsNumber];
+MemorySegment* memorySections[maxRequestsNumber];
+int memorySize, requestsCount, memorySectionsCount; 
+int freeSectionsCount, operationStatus[maxRequestsNumber];
 
-void add_block(int id, int size) {
-    free_blocks_id.insert( { id, size } );
-    free_blocks_size.insert( { size, id } );
+void swap(int var_a, int var_b) {
+    MemorySegment* t = heap[var_a];
+    heap[var_a] = heap[var_b];
+    heap[var_b] = t;
+    heap[var_a]->id = var_a;
+    heap[var_b]->id = var_b;
 }
 
-void remove(map<int, int>::iterator& a) {
-    auto b = free_blocks_size.find(a -> second);
-    while (b -> second != a -> first) b++;
-    free_blocks_size.erase(b);
-    free_blocks_id.erase(a);
+bool greaterThan(int var_a, int var_b) {
+    return (heap[var_a]->end - heap[var_a]->start) >
+    (heap[var_b]->end - heap[var_b]->start);
+}
+
+void heapify(int id) {
+    while (true) {
+        int q = id;
+        if (((id << 1) + 1 < freeSectionsCount) && greaterThan((id << 1) + 1, q)) {
+            q = (id << 1) + 1;
+        }
+        if (((id << 1) + 2 < freeSectionsCount) && greaterThan((id << 1) + 2, q)) {
+            q = (id << 1) + 2;
+        }
+        if (id == q) {
+            return;
+        }
+        swap(id, q);
+        id = q;
+    }
+}
+
+void heapLift(int id) {
+    while (id && greaterThan(id, (id - 1) >> 1)) {
+        swap(id, (id - 1) >> 1);
+        id = (id - 1) >> 1;
+    }
+}
+
+void heapRemove() {
+    freeSectionsCount--;
+    if (freeSectionsCount > 0) {
+        swap(0, freeSectionsCount);
+        heapify(0);
+    }
+}
+
+void heapRemove(int id) {
+    swap(id, freeSectionsCount - 1);
+    freeSectionsCount--;
+    if (id >= freeSectionsCount) {
+        return;
+    }
+    heapLift(id);
+    heapify(id);
+}
+
+void heapAdd(MemorySegment* a) {
+    a->id = freeSectionsCount;
+    heap[freeSectionsCount] = a;
+    heapLift(freeSectionsCount);
+    freeSectionsCount++;
+}
+
+void allocateMemory(int size) {
+    MemorySegment* c = heap[0];
+    if (freeSectionsCount == 0 || ((c->end - c->start) < size)) {
+        operationStatus[memorySectionsCount++] = 0;
+        cout << "-1\n";
+        return;
+    }
+    operationStatus[memorySectionsCount++] = 1;
+    memorySections[memorySectionsCount - 1] = new MemorySegment(c->prev, c, false, c->start, c->start + size, -1);
+    cout << 1 + c->start << "\n";
+    c->start += size;
+    if (c->start < c->end) {
+        heapify(c->id);
+    } else {
+        c->remove();
+        heapRemove();
+        delete c;
+    }
+}
+
+void freeMemory(int id) {
+    id--;
+    operationStatus[memorySectionsCount++] = 2;
+    if (operationStatus[id] == 0) {
+        return;
+    }
+    operationStatus[id] = 2;
+    MemorySegment* c = memorySections[id];
+    MemorySegment* sp = c->prev;
+    MemorySegment* sn = c->next;
+    bool bp = (sp != nullptr && sp->free);
+    bool bn = (sn != nullptr && sn->free);
+    if (!bp && !bn) {
+        c->free = true;
+        heapAdd(c);
+        return;
+    }
+    if (!bp) {
+        sn->start = c->start;
+        heapLift(sn->id);
+        c->remove();
+        delete c;
+        return;
+    }
+    if (!bn) {
+        sp->end = c->end;
+        heapLift(sp->id);
+        c->remove();
+        delete c;
+        return;
+    }
+    sp->end = sn->end;
+    heapLift(sp->id);
+    c->remove();
+    delete c;
+    heapRemove(sn->id);
+    sn->remove();
+    delete sn;
 }
 
 int main() {
-    int max_size, req_count, id, size;
-    cin >> max_size >> req_count;
-    request_entry requests[req_count];
-    add_block(1, max_size);
-
-    for (int curr_req_id = 0; curr_req_id < req_count; curr_req_id++) {
-        int input; cin >> input;
-
-        if (input > 0) {
-            auto selected_block = free_blocks_size.lower_bound(input);
-            if (selected_block == free_blocks_size.end()) id = -1;
-            else {
-                id = selected_block->second;
-                int new_id = id + input;
-                size = selected_block->first - input;
-
-                free_blocks_id.erase(selected_block->second);
-                free_blocks_size.erase(selected_block);
-
-                if (size != 0) add_block(new_id, size);
-            }
-            cout << id << endl;
-
-        }
-        else {
-            int id_x = requests[abs(input) - 1].id;
-            int size_x = requests[abs(input) - 1].size;
-            if (id_x == -1) continue;
-
-            auto right = free_blocks_id.lower_bound(id_x);
-            auto left = (right != free_blocks_id.begin()) ? prev(right) : free_blocks_id.end();
-
-            if (right != free_blocks_id.end() && right->first == id_x + size_x){
-                if (left != free_blocks_id.end() && left->first + left->second == id_x) {
-                    id = left->first;
-                    size = left->second + right->second;
-                    remove(left);
-                    remove(right);
-                    add_block(id, size + size_x);
-                } else {
-                    size = right->second;
-                    remove(right);
-                    add_block(id_x, size + size_x);
-                }
-            } else {
-                if (left != free_blocks_id.end() && left->first + left->second == id_x) {
-                    id = left->first;
-                    size = left->second;
-                    remove(left);
-                    add_block(id, size + size_x);
-                } else add_block(id_x, size_x);
-            }
-        }
-        requests[curr_req_id] = {input, id};
+    cin >> memorySize >> requestsCount;
+    freeSectionsCount = 1;
+    heap[0] = new MemorySegment(nullptr, nullptr, true, 0, memorySize, 0);
+    int t;
+    for (int i = 0; i < requestsCount; i++) {
+        cin >> t;
+        if (t > 0)
+            allocateMemory(t);
+        else
+            freeMemory(-t);
     }
+    return 0;
 }
